07db609602c1a69ed9d49b8b5eb902609fc16716
InfraAiProxy <Donbottech@proton.me>
2024-08-11 06:44:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,8 @@ const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 400,
+    height: 400,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
